95613ec85121260fcd28ad951ed513c30f01eae5
Luan Mai <luan.mai@lonsec.com.au>
2015-08-13 15:02:04 +1000

    etf_upload.py: updated

--- etf_upload.py
+++ etf_upload.py
@@ -128,13 +128,27 @@ def upload(db, excel_file, sheet_name_or_idx, data_provider):
     date_value = db.get_one_value('select top 1 [date] from {}'.format(tt_name))
 
     if not isinstance(date_value, datetime.datetime):
-        logger.info('Convert varchar to datetime for [date] column')
+        if isinstance(date_value, str):
+            logger.info('Convert varchar to datetime for [date] column')
 
-        # update table to yyyy-mm-dd format before convert to datetime type
-        db.execute('''
-            update {}
-            set [date]=right([date],4)+'-'+SUBSTRING([date], 4, 2) + '-' + left([date],2)
-        '''.format(tt_name))
+            # update table to yyyy-mm-dd format before convert to datetime type
+            db.execute('''
+                update {}
+                set [date]=right([date],4)+'-'+SUBSTRING([date], 4, 2) + '-' + left([date],2)
+            '''.format(tt_name))
+        elif isinstance(date_value, float):
+            logger.info('Convert float to datetime for [date] column')
+            # SQL Server counts its dates from 01/01/1900 and Excel from 12/30/1899 = 2 days less.
+            # update table to yyyy-mm-dd format before convert to datetime type
+            db.execute('''
+                alter table {tt_name}
+                alter column [date] varchar(20)
+            '''.format(tt_name=tt_name))
+
+            db.execute('''
+                update {tt_name}
+                set date=cast(date - 2 as datetime)
+            '''.format(tt_name=tt_name))
 
         db.execute('''
             alter table {}
